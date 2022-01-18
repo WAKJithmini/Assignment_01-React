0d4996d29fe0cc3f8afdc16c31db9ccff67355a5
Kalasi Jithmini <kjithmini@gmail.com>
2022-01-18 08:21:49 +0530

    changes in login form

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, {useState} from 'react';
 import LoginForm from './component/LoginForm';
 function App() {
   const adminUser ={ // the place where we chack the login details
- email:"admin@gmail.com",
- password:"admin@123"
+   password:"admin@123"
   }
 
-  const [user, setUser] = useState({name:"",email:""});
+  const [user, setUser] = useState({name:""});
   const [error, setError] = useState("");
 
   const Login =details =>{
     console.log(details);
 
-    if(details.email === adminUser.email && details.password === adminUser.password){
+    if(details.password === adminUser.password){
       console.log("Logged in");
       setUser({
-        name:details.name,
-        email:details.email
+        name:details.name
       });
     }else{
       console.log("details don't match");
@@ -25,13 +23,13 @@ function App() {
   }
 
   const Logout =() => {
-    setUser({name:"",email:""});
+    setUser({name:""});
     setError("");
   }
 
   return (
     <div className="App">
-      {(user.email !=="") ? (
+      {(user.name !=="") ? (
         <div className="welcome">
           <h2>welcome ,<span>{user.name}</span></h2>
           <button onClick={Logout}>Logout</button>
--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 function LoginForm({ Login , error}) {
-    const [details, setDetails] = useState({name:"",email:"", password:""});
+    const [details, setDetails] = useState({name:"", password:""});
     
     const submitHandler = e =>{
          e.preventDefault();
@@ -16,12 +16,8 @@ function LoginForm({ Login , error}) {
                 <h2>Login</h2>
                 {(error !="") ? ( <div className="error">{error}</div>):""}
                 <div className="form-group">
-                    <label htmlFor="name">Name:</label>
+                    <label htmlFor="name">Username:</label>
                     <input type="text" name="name" id="name" onChange={e =>setDetails({...details, name: e.target.value})} value={details.name} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">email:</label>
-                    <input type="email" name="email" id="email" onChange={e =>setDetails({...details, email: e.target.value})} value={details.email} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
